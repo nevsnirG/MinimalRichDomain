3a330a8eb60e7fd2246a47ce540b67a3088447a8
Ken <ken@vgss.io>
2023-12-06 21:25:44 +0100

    Change Apply method to properly increment version and append domain events

--- src/MinimalRichDomain/Entity.cs
+++ src/MinimalRichDomain/Entity.cs
@@ -1,4 +1,5 @@
 ﻿using MinimalDomainEvents.Core;
+using System.Reflection;
 
 namespace MinimalRichDomain;
 public abstract class Entity<TId>
@@ -18,40 +19,18 @@ public abstract class Entity<TId>
     protected Entity(TId id, IReadOnlyCollection<IDomainEvent> domainEvents)
     {
         Id = id;
+        _domainEvents = new(domainEvents.Count);
         Rehydrate(domainEvents);
-        _domainEvents = new(domainEvents);
     }
 
     protected virtual void Rehydrate(IReadOnlyCollection<IDomainEvent> domainEvents)
     {
-        foreach (var @event in domainEvents.OrderBy(de => de.Version))
+        foreach (var domainEvent in domainEvents.OrderBy(de => de.Version))
         {
-            Apply(@event);
+            Apply(domainEvent);
         }
 
         ValidateRehydration();
-    }
-
-    protected void Apply(IDomainEvent @event)
-    {
-        if (CanApply(@event))
-        {
-            var eventType = @event.GetType();
-            var interfaceType = typeof(IApplyEvent<>).MakeGenericType(eventType);
-
-            var applyMethod = GetType().GetInterfaceMap(interfaceType).TargetMethods
-                .FirstOrDefault(m => m.Name.EndsWith(nameof(IApplyEvent<IDomainEvent>.Apply)));
-
-            applyMethod?.Invoke(this, new object[] { @event });
-            IncrementVersion();
-        }
-        else
-            throw new InvalidOperationException($"Cannot apply event with version {@event.Version} to entity version {CurrentVersion}. Some history might be missing.");
-    }
-
-    private bool CanApply(IDomainEvent @event)
-    {
-        return @event.Version == NextVersion;
     }
 
     protected abstract void ValidateRehydration();
@@ -63,11 +42,43 @@ public abstract class Entity<TId>
 
         Apply(domainEvent);
         DomainEventTracker.RaiseDomainEvent(domainEvent);
+    }
+
+    public void Apply(IDomainEvent domainEvent)
+    {
+        if (CanApply(domainEvent))
+        {
+            var eventType = domainEvent.GetType();
+            var interfaceType = typeof(IApplyEvent<>).MakeGenericType(eventType);
+
+            var applyMethod = GetType().GetInterfaceMap(interfaceType).TargetMethods
+                .FirstOrDefault(m => m.Name.EndsWith(nameof(IApplyEvent<IDomainEvent>.Apply)));
+
+            if (applyMethod is not default(MethodInfo))
+            {
+                applyMethod.Invoke(this, new object[] { domainEvent });
+                AppliedDomainEvent(domainEvent);
+                return;
+            }
+        }
+
+        throw new InvalidOperationException($"Cannot apply event with version {domainEvent.Version} to entity version {CurrentVersion}. Some history might be missing.");
+    }
+
+    private bool CanApply(IDomainEvent @event)
+    {
+        return @event.Version == NextVersion;
+    }
+
+    private void AppliedDomainEvent(IDomainEvent domainEvent)
+    {
         _domainEvents.Add(domainEvent);
+        IncrementVersion();
     }
 
     private void IncrementVersion()
     {
         CurrentVersion++;
     }
+
 }
