3014daaadf3114e31606a6b5ee1be2535a29229d
Ken <ken@vgss.io>
2023-12-06 21:42:34 +0100

    Add unit tests

---
+++ test/MinimalRichDomain.Tests/EntityTests.cs
@@ -0,0 +1,67 @@
+using FluentAssertions;
+
+namespace MinimalRichDomain.Tests;
+
+public class EntityTests
+{
+    [Fact]
+    public void CannotApplyEventWithLowerVersion()
+    {
+        var domainEvent = new TestDomainEvent(-1);
+        var testEntity = new TestEntity();
+
+        var action = () => testEntity.Apply(domainEvent);
+
+        action.Should().ThrowExactly<InvalidOperationException>().WithMessage("Cannot apply event with version -1 to entity version 0. Some history might be missing.");
+    }
+
+    [Fact]
+    public void CannotApplyEventWithSameVersion()
+    {
+        var domainEvent = new TestDomainEvent(0);
+        var testEntity = new TestEntity();
+
+        var action = () => testEntity.Apply(domainEvent);
+
+        action.Should().ThrowExactly<InvalidOperationException>().WithMessage("Cannot apply event with version 0 to entity version 0. Some history might be missing.");
+    }
+
+    [Fact]
+    public void CannotApplyEventWithTooHighVersion()
+    {
+        var domainEvent = new TestDomainEvent(2);
+        var testEntity = new TestEntity();
+
+        var action = () => testEntity.Apply(domainEvent);
+
+        action.Should().ThrowExactly<InvalidOperationException>().WithMessage("Cannot apply event with version 2 to entity version 0. Some history might be missing.");
+    }
+
+    [Fact]
+    public void CanApplyEventWithIncrementalVersion()
+    {
+        var domainEvent = new TestDomainEvent(1);
+        var testEntity = new TestEntity();
+
+        testEntity.Apply(domainEvent);
+
+        testEntity.CurrentVersion.Should().Be(1);
+    }
+
+    private sealed record class TestDomainEvent(int Version) : IDomainEvent;
+
+    private class TestEntity : Entity<Guid>, IApplyEvent<TestDomainEvent>
+    {
+        public TestEntity() : base(Guid.NewGuid())
+        {
+        }
+
+        protected override void ValidateRehydration()
+        {
+        }
+
+        void IApplyEvent<TestDomainEvent>.Apply(TestDomainEvent @event)
+        {
+        }
+    }
+}
